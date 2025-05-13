8f3ad40786459101a571e3c8ae5af94affb4e4b4
Noah Matthieu Abi Chahla <noah.abichahlapro@gmail.com>
2025-05-12 21:55:01 -0700

    Update HomeViewModel.swift

--- App/Group-Project/Group-Project/ViewModels/HomeViewModel.swift
+++ App/Group-Project/Group-Project/ViewModels/HomeViewModel.swift
@@ -11,23 +11,22 @@ class HomeViewModel: ObservableObject {
     @Published var isLoading = false
     @Published var error: Error?
     
-    // Breaking news feeds for the home view
-    let breakingNewsFeeds = [
-        "Progressive": "https://progressive.org/magazine/rss-feeds/",
-        "OpEdNews": "https://www.opednews.com/feeds/rss.xml",
-        "Al Jazeera": "https://www.aljazeera.com/xml/rss/all.xml",
-        "France24": "https://www.france24.com/en/rss",
-        "GlobalIssues": "https://www.globalissues.org/news/feed"
+    // Local news feeds (copied from NewsViewModel)
+    let localNewsFeeds = [
+        "Mission Local": "https://missionlocal.org/feed",
+        "SF Bay View": "https://sfbayview.com/feed",
+        "SF Public Press": "https://www.sfpublicpress.org/feed",
+        "48 Hills": "https://48hills.org/feed"
     ]
     
-    // Fetch breaking news for home
+    // Fetch local news for carousel
     func fetchCarouselNews() async {
         isLoading = true
         error = nil
         var allArticles: [RSSItem] = []
         
         await withTaskGroup(of: [RSSItem]?.self) { group in
-            for (_, feed) in breakingNewsFeeds {
+            for (_, feed) in localNewsFeeds {
                 group.addTask {
                     let service = RSSService()
                     do {
@@ -57,7 +56,7 @@ class HomeViewModel: ObservableObject {
             self.carouselArticles = Array(limitedArticles)
             self.isLoading = false
         }
-        print("Fetched \(self.carouselArticles.count) breaking news articles with images for carousel")
+        print("Fetched \(self.carouselArticles.count) local news articles with images for carousel")
     }
     
     // Convert RSSItems to ArticleItems for the carousel
